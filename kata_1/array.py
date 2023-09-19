778f8d685ce49926eecf41807dc18b6424c44f07
Cristian Mello <cristianc.mello@gmail.com>
2023-09-19 16:30:26 -0300

    Adding comment in generate_codes_using_genexp (array example)

--- kata_1/array.py
+++ kata_1/array.py
@@ -2,5 +2,4 @@ import array
 
 
 def generate_codes_using_genexp(symbols):
-    return array.array('I', (ord(c) for c in symbols))
-
+    return array.array('I', (ord(c) for c in symbols))  # second arg: () are required
