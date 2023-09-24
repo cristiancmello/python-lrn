da842347a293bc9ec6d0342db2f034f7f498b3a5
Cristian Mello <cristianc.mello@gmail.com>
2023-09-24 13:30:34 -0300

    Updating array impl

--- kata_1/array.py
+++ kata_1/array.py
@@ -1,5 +1,5 @@
-import array
+from array import array
 
 
-def generate_codes_using_genexp(symbols):
-    return array.array('I', (ord(c) for c in symbols))  # second arg: () are required
+def generate_codes_using_genexp(symbols: str):
+    return array('I', (ord(s) for s in symbols))  # second arg: () are required
--- tests/test_array.py
+++ tests/test_array.py
@@ -1,10 +1,9 @@
-import array
 import unittest
 
-from kata_1.array import generate_codes_using_genexp
+from kata_1.array import *
 
 
 class TestArray(unittest.TestCase):
     def test_get_codes_from_symbols_more_eff_using_genexp(self):
         symbols = '$¢£¥€¤'
-        self.assertEqual(generate_codes_using_genexp(symbols), array.array('I', [36, 162, 163, 165, 8364, 164]))
+        self.assertEqual(generate_codes_using_genexp(symbols), array('I', [36, 162, 163, 165, 8364, 164]))
