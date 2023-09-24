cfe0636c1a511905f87add93ee093b6d10bf5de4
Cristian Mello <cristianc.mello@gmail.com>
2023-09-24 13:22:08 -0300

    Fixing list and tuple impl

--- kata_1/list.py
+++ kata_1/list.py
@@ -4,7 +4,7 @@ list_is_container_mutable_sequence = []
 
 an_ordered_list = ['orange', 'apple', 'grape']
 
-heterogeneous_list = ['string', 2.09, (1, 2), array.array('d', [1, 2]), list(), {'name': 'John'}]
+heterogeneous_list = ['str', 0.99, (1, 2), array.array('I', [1, 2]), [2], {'name': 'John'}]
 
 list_created_by_literals = ['banana', 'apple']
 
@@ -24,51 +24,51 @@ list_created_using_constructor_w_dict_items = list({'name': 'John', 'city': 'New
 list_created_using_constructor_w_string = list('abc')
 
 
-def append_element_on_list(element):
-    list_is_container_mutable_sequence.append(element)
+def append_element_on_list(e):
+    list_is_container_mutable_sequence.append(e)
 
 
-def list_shallow_copy(letters):
-    return letters[::]
+def list_shallow_copy(letters: list):
+    return letters[:]
 
 
-def list_slicing_from_2_index_to_end(letters):
+def list_slicing_from_2_index_to_end(letters: list):
     return letters[2:]
 
 
-def list_slicing_uppercase_letters(letters):
+def list_slicing_uppercase_letters(letters: list):
     return letters[::2]
 
 
-def list_slicing_lowercase_letters(letters):
+def list_slicing_lowercase_letters(letters: list):
     return letters[1::2]
 
 
-def list_slicing_first_three_letters(letters):
+def list_slicing_first_three_letters(letters: list):
     return letters[:3]
 
 
-def list_slicing_last_letter(letters):
+def list_slicing_last_letter(letters: list):
     return letters[-1:]
 
 
-def list_slicing_last_three_lowercase_letters(letters):
+def list_slicing_last_three_lowercase_letters(letters: list):
     return letters[-5::2]
 
 
-def list_slicing_medium_uppercase_letters(letters):
+def list_slicing_medium_uppercase_letters(letters: list):
     return letters[2:5:2]
 
 
-def list_slicing_reverse_all_letters(letters):
+def list_slicing_reverse_all_letters(letters: list):
     return letters[::-1]
 
 
-def list_slicing_uppercase_using_slice_constructor(letters):
-    return letters[::2]
+def list_slicing_uppercase_using_slice_constructor(letters: list):
+    return letters[slice(None, None, 2)]
 
 
-def generate_code_list_using_forloop(symbols):
+def generate_code_list_using_forloop(symbols: str) -> list[int]:
     codes = []
 
     for s in symbols:
@@ -77,45 +77,42 @@ def generate_code_list_using_forloop(symbols):
     return codes
 
 
-def generate_code_list_using_listcomps(symbols):
+def generate_code_list_using_listcomps(symbols: str) -> list[int]:
     return [ord(s) for s in symbols]
 
 
-def generate_code_list_using_listcomps_without_walrusop(symbols):
-    return [ord(c) for c in symbols]
+def generate_code_list_using_listcomps_without_walrusop(symbols: str):
+    return [ord(s) for s in symbols]
 
 
-def generate_code_list_using_listcomps_walrusop(symbols):
-    codes = [last := ord(c) for c in symbols]
-
-    return last, codes
-
-
-def beyond_ascii_listcomps(symbols):
-    return [ord(c) for c in symbols if ord(c) > 127]
+def generate_code_list_using_listcomps_walrusop(input: str) -> tuple:
+    codes = [latest := ord(s) for s in input]
+    return latest, codes
 
 
-def beyond_ascii_mapfilter(symbols):
-    return list(filter(lambda c: c > 127, map(ord, symbols)))
+def beyond_ascii_listcomps(symbols: str) -> list[int]:
+    return [ord(s) for s in symbols if ord(s) > 127]
 
 
-def cartesian_product_tshirts(colors, sizes):
+def beyond_ascii_mapfilter(symbols: str):
+    return list(filter(lambda s: s > 127, map(ord, symbols)))
+
+
+def cartesian_product_tshirts(colors: list[str], sizes: list[str]) -> list[tuple[str, str]]:
     return [(color, size) for color in colors for size in sizes]
 
 
-def cartesian_product_tshirts_nestedloop(colors, sizes):
+def cartesian_product_tshirts_nestedloop(colors: list[str], sizes: list[str]) -> list[tuple[str, str]]:
     tshirts = []
-
     for color in colors:
         for size in sizes:
             tshirts.append((color, size))
-
     return tshirts
 
 
-def cartesian_product(l1, l2):
+def cartesian_product(l1: list[int], l2: list[int]) -> list[int]:
     return [a * b for a in l1 for b in l2]
 
 
-def generate_codes_using_genexp(symbols):
-    return list(ord(c) for c in symbols)
+def generate_codes_using_genexp(symbols: str) -> list[int]:
+    return list(ord(s) for s in symbols)
--- kata_1/tuple.py
+++ kata_1/tuple.py
@@ -5,41 +5,38 @@ lax_coordinates = (33.9425, -118.408056)
 tokyo_data_tuple_as_record = ('Tokyo', 2003, 32_450, 0.66, 8014)
 
 
-def generate_sorted_country_passports_using_formmating_op(traveler_ids):
-    str_country_passports = ""
-
-    for passport in sorted(traveler_ids):
-        str_country_passports += '%s/%s\n' % passport
-
-    return str_country_passports
+def generate_sorted_country_passports_using_formmating_op(passports):
+    passport_str = ''
+    for passport in sorted(passports):
+        passport_str += '%s/%s\n' % passport
+    return passport_str
 
 
-def generate_sorted_countries_using_listcomp_dummyvar(traveler_ids):
-    # _ : called dummy variable
-    return list(country for country, _ in sorted(traveler_ids))
+def generate_sorted_countries_using_listcomp_dummyvar(passports):
+    return [country for country, _ in sorted(passports)]
 
 
-def generate_sorted_countries_using_forloop_dummyvar(traveler_ids):
-    sorted_countries = []
+def generate_sorted_countries_using_forloop_dummyvar(passports):
+    countries = []
 
-    for country, _ in sorted(traveler_ids):
-        sorted_countries.append(country)
+    for country, _ in sorted(passports):
+        countries.append(country)
 
-    return sorted_countries
+    return countries
 
 
-def verify_tuples_are_equals(a, b):
-    return a == b
+def verify_tuples_are_equals(t1, t2):
+    return t1 == t2
 
 
 def verify_tuples_are_equals_after_insert_mutable_obj(a, b):
-    b[-1].append(99)
+    b[2].append(99)
     return a == b
 
 
-def fixed(o):
+def fixed(t: tuple):
     try:
-        hash(o)
+        hash(t)
     except TypeError:
         return False
     return True
@@ -53,77 +50,76 @@ def create_list_using_constructor(l):
     return list(l)
 
 
-def appending_element_in_tuple(t, element):
-    l_ = list(t)
-    l_.append(element)
-    return tuple(l_)
+def appending_element_in_tuple(t, e):
+    _l = list(t)
+    _l.append(e)
+    return tuple(_l)
 
 
-def concats(t1, t2):
+def concats(t1: tuple, t2: tuple) -> tuple:
     return t1 + t2
 
 
-def has_element(t, e):
+def has_element(t: tuple, e: any) -> bool:
     return e in t
 
 
-def count_elements(t: tuple, e):
+def count_elements(t: tuple, e: any) -> int:
     return t.count(e)
 
 
-def get_value_from_tuple(t, p):
+def get_value_from_tuple(t: tuple, p: int) -> any:
     return t[p]
 
 
-def get_first_occurr(t: tuple, e):
+def get_first_occurr(t: tuple, e: any) -> int:
     return t.index(e)
 
 
-def get_tuple_length(t: tuple):
+def get_tuple_length(t: tuple) -> int:
     return len(t)
 
 
-def repeat_concat(t):
+def repeat_concat(t: tuple) -> tuple:
     return t * 2
 
 
-def reversed_repeated_concat(t):
+def reversed_repeated_concat(t: tuple) -> tuple:
     return 2 * t
 
 
-def get_coord_unpacking_tuple(t, coord):
-    if coord == 'lat':
-        lat, _ = t
+def get_coord_unpacking_tuple(coord: tuple[float, float], attr: str) -> float:
+    if attr == 'lat':
+        lat, _ = coord
         return lat
-    elif coord == 'long':
-        _, long = t
+    elif attr == 'long':
+        _, long = coord
         return long
 
 
-def powerful_swap_using_unpack(t):
-    lat, long = t
+def powerful_swap_using_unpack(coord: tuple[float, float]) -> tuple[float, float]:
+    lat, long = coord
     long, lat = lat, long
-
     return lat, long
 
 
-def unpacking_tuple_as_arg_for_divmod(t: tuple) -> tuple:
-    return divmod(*t)
+def unpacking_tuple_as_arg_for_divmod(args: tuple[float, float]) -> tuple[float, float]:
+    return divmod(*args)
 
 
-def get_basename_using_unpacking_ospathsplit(file):
-    basename, _ = os.path.split(file)
+def get_basename_using_unpacking_ospathsplit(filename: str) -> str:
+    basename, _ = os.path.split(filename)
     return basename
 
 
 def grab_excess_items(t: tuple):
-    a, b, *rest = t
-    return a, b, rest
+    a, b, *tail = t
+    return a, b, tail
 
 
 def grab_excess_items_medium_pos(t: tuple):
-    a, *body, c, d = t
-    return a, body, c, d
+    a, *medium, c, d = t
+    return a, medium, c, d
 
 
 def grab_excess_items_header_pos(t: tuple):
@@ -131,5 +127,5 @@ def grab_excess_items_header_pos(t: tuple):
     return header, b, c, d
 
 
-def generate_codes_using_genexp(symbols):
-    return tuple(ord(c) for c in symbols)
+def generate_codes_using_genexp(symbols: str) -> tuple[int]:
+    return tuple(ord(s) for s in symbols)
